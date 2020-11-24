ece57fcc137fa7e7666c05241cbe91e79f7f65ae
NORDIC\pkarim <paria.karim@gmail.com>
2020-11-24 15:43:20 +0100

    added TranslatorTable to profile

---
+++ src/components/TranslatorTable.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TranslatorTable = props => {
+
+    return (
+        <div>
+            <h4>Saved Translations</h4>
+            <ul>
+                <li>List item</li>
+            </ul>
+        </div>
+        
+    )
+}
+
+export default TranslatorTable;
