3ced24ac8220dcbde4495eb4fc53e192920423be
ludwigcarlsson <zhendiac@gmail.com>
2020-11-25 15:30:24 +0100

    feat: added root route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 import Login from './components/containers/Login'
 import Translator from './components/containers/Translator'
 import Profile from './components/containers/Profile'
 
 function App() {
+
   return (
     <Router>
       <div className="App">
         <div className="App-header">
           <Switch>
-            <Route path="/login" component={ Login }/>
-            <Route path="/translator" component={ Translator }/>
-            <Route path="/profile" component={ Profile }/>
+            <Route exact path="/login" component={ Login }/>
+            <Route exact path="/translator" component={ Translator }/>
+            <Route exact path="/profile" component={ Profile }/>
+            <Redirect from="/" to="/login" />
           </Switch>
         </div>
       </div>
