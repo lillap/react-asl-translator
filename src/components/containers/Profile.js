48f8f1bb63bc57cfdbf977ac44196f9acb4fa326
ludwigcarlsson <zhendiac@gmail.com>
2020-11-25 14:21:09 +0100

    feat: added redirect component and validate input on translationpage

--- src/components/containers/Profile.js
+++ src/components/containers/Profile.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { clearStorage } from '../../utils/localstorage';
+import Redirect from '../redirect';
 import TranslatorTable from '../ui/TranslatorTable';
 
-// create some sort of link to translator-page and a logout/clear localstorage-button
+// create some sort of link to translator-page
 
 const Profile = () => {
-
-    const history = useHistory();
-
-    const logOut = () => {
-        clearStorage();
-        history.replace('/login');
-    }
 
     return (
         <div>
             <h1>Profile</h1>
             <TranslatorTable/>
-            <button type="button" onClick={logOut}> clear </button>
+            <Redirect />
         </div>
        
     )
--- src/components/forms/TranslatorForm.js
+++ src/components/forms/TranslatorForm.js
@@ -3,23 +3,36 @@ import React, { useState } from 'react';
 const TranslatorForm = props => {
 
     const [translation, setTranslation] = useState('');
+    const [inputError, setInputError] = useState(false);
     
     const onTranslateClick = () => {
+
         props.makeTranslation(translation); 
     }
 
     // validate if allowed characters and if allowed length
-    const onTranslatorChange = event => setTranslation(event.target.value); 
+    const onTranslatorChange = event => {
+        let value = event.target.value;
+
+        if(!value.match(/^[A-Za-z]+$/)) {
+            setInputError(true);
+        } else {
+            setInputError(false);
+            setTranslation(value);
+        }
+    }; 
     
     return (
         <form>
             <div>
                 <label>Translate: </label>
-                <input type="text" placeholder="Enter your word to translate..." onChange={onTranslatorChange}/>
+                <input type="text" placeholder="Enter your word to translate..."
+                    onChange={onTranslatorChange} maxLength={40}/>
             </div>
             <div>
-                <button type="button" onClick={onTranslateClick}>Translate!</button>
+                <button type="button" disabled={inputError} onClick={onTranslateClick}>Translate!</button>
             </div>
+            {inputError && <p>This form only accepts a-zA-Z</p>}
         </form> 
     )
 }
